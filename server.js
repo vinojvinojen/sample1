6b38a185a32762af58bc00cde2d88f85fe22f08a
vinojvinojen <vinoj455@gmail.com>
2023-11-01 20:06:11 +0530

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-const express = require('express');
-const cors = require('cors'); // Import the cors module
+const jsonserver = require('json-server');
+const cors = require('cors'); // Import the cors 
+const server = jsonserver.create();
+const router=jsonserver.router("./db.json");
+const middleware=jsonserver.defaults (
+    {
+        static:"./build"
+    }
+)
+
 
 const app = express();
 
@@ -9,7 +17,15 @@ app.use(cors());
 // Your other Express app configurations and routes go here
 
 // Start your Express server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const port = process.env.PORT || 5000;
+server.use(middleware);
+server.use(
+    jsonserver.rewriter({
+        "/api/*":"/$1",
+    })
+)
+
+server.use(router)
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
